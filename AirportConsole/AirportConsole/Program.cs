2461492c12032cb2339eb128bb3b4ca522860a80
Anton <anton.babich@gmail.com>
2016-07-11 21:07:46 +0300

    Added enetering Flight from console (not finished)

--- AirportConsole/AirportConsole/Program.cs
+++ AirportConsole/AirportConsole/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace AirportConsole
 {
     class Program
     {
+        //Spent time 10.07 3 hours
+        //11.07 20:00 
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.White;
+            // Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Welcome to airport flight manager!");
             FlyightsManager flyightsManager = new FlyightsManager();
             flyightsManager.InitiolizeDefaultStructure();
@@ -31,7 +34,8 @@ namespace AirportConsole
                     try
                     {
                         numberMenu = (MainMenuVariants)short.Parse(Console.ReadLine());
-                    }catch
+                    }
+                    catch
                     {   // Writhing warning message:
                         Console.ForegroundColor = ConsoleColor.Red;
 
@@ -43,15 +47,15 @@ namespace AirportConsole
                     {
 
                         case MainMenuVariants.Exit:
-                            Console.BackgroundColor = ConsoleColor.Blue;
+                            // Console.BackgroundColor = ConsoleColor.Blue;
                             Console.WriteLine("Are you sure you would like to exit? Please enter Y, in other case please press any key");
                             string answer = Console.ReadLine();
                             if (answer == "Y")
                             {
                                 customerWantExit = true;
-                                
+
                             }
-                            
+
                             break;
                         case MainMenuVariants.AddFlight:
                             flyightsManager.AddFlightFromConsole();
@@ -62,13 +66,13 @@ namespace AirportConsole
 
                         default:
                             // Writhing warning message:
-                            Console.ForegroundColor = ConsoleColor.Red;
+                            // Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("You have selected wrong menu number, please try again");
-                      
+
 
                             break;
                     }
-  
+
                 }
             }
         }
@@ -77,7 +81,7 @@ namespace AirportConsole
     enum MainMenuVariants
     {
         Exit = 0,
-        AddFlight =1,
+        AddFlight = 1,
         DeleteFlight = 2,
         PrintAllFlights = 3,
         SearchByNumber = 4,
@@ -111,7 +115,8 @@ namespace AirportConsole
         }
 
     }
-    class FlyightsManager {
+    class FlyightsManager
+    {
 
         List<Flight> listOfFlights = new List<Flight>();
         public void InitiolizeDefaultStructure()
@@ -120,7 +125,7 @@ namespace AirportConsole
         }
         public bool AddFlightFromConsole()
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            // Console.ForegroundColor = ConsoleColor.Green;
             // Processing Number (could be 3 situation, incorrect number, existed number or correct number)
             Console.WriteLine($"Please enter Number of flihgt or enter {(short)MainMenuVariants.Exit}-if you would like exit from adding Flight");
             int numberOfFlight = 0;
@@ -128,38 +133,118 @@ namespace AirportConsole
             bool customerWantExit = false;
             do
             {
-                try
+                if (ReadIntValueFromConsole(ref numberOfFlight))
                 {
-                    numberOfFlight = int.Parse(Console.ReadLine());
-                    if (numberOfFlight == (int)MainMenuVariants.Exit)
-                        customerWantExit = true;
-                    if (numberOfFlight > 0)
+                    if (GetFlyightByNumber(numberOfFlight) == null)
                     {
-                        if (GetFlyightByNumber(numberOfFlight) == null)
-                        {
-                            numberIsCorrect = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Number which you entered already used, please try again or enter {(short)MainMenuVariants.Exit}-if you would like exit from adding Flight");
-                        }
+                        numberIsCorrect = true;
                     }
-                }
-                catch
-                {   // Writhing warning message:
-                    Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                    {
+                        Console.WriteLine($"Number which you entered already used, please try again or enter {(short)MainMenuVariants.Exit}-if you would like exit from adding Flight");
+                    }
 
-                    Console.WriteLine($"You have entered wrong number, please try again or enter {(short)MainMenuVariants.Exit}-if you would like exit from adding Flight");
-                    Console.ForegroundColor = ConsoleColor.Green;
                 }
-
+                else
+                {
+                    customerWantExit = true;
+                }
             } while (!numberIsCorrect && !customerWantExit);
 
             // Entering other properties
+            if (numberIsCorrect)
+            {
+                Flight addFlight = new Flight();
+
+                // Fill all other details
+                if (FillDataFromConsole(1, addFlight) && FillDataFromConsole(2, addFlight) && FillDataFromConsole(3, addFlight) && FillDataFromConsole(4, addFlight) && FillDataFromConsole(4, addFlight))
+                {
+                    listOfFlights.Add(addFlight);
+                    // TODO: Implement printing of added flyght
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                /*
+                 *  public DateTime DateTimeOfArrival { get; set; }
+                public int Number { get; set; }
+                public string City { get; set; }
+                public string Airline { get; set; }
+                public int Terminal { get; set; }
+                public FlightStatus Status { get; set; }*/
+            }
+
+            return false;
+        }
+        private bool FillDataFromConsole(int indexOfPropperty, Flight updatedFlight)
+        {
+            string nameOfValue = "";
+            switch (indexOfPropperty)
+            {
+                case (1)://City
+                    nameOfValue = "City";
+
+                    break;
+                case (3)://Terminal
+                    nameOfValue = "Terminal";
+
+                    break;
+            }
+            Console.WriteLine($"Please enter value of:{nameOfValue}");
+            string enteredValue = Console.ReadLine();
+            switch (indexOfPropperty)
+            {
+                case (1)://City
+                    updatedFlight.City = enteredValue;
+
+                    break;
+                case (3)://Terminal
+                    int returnValue = 0;
+                    if (ReadIntValueFromConsole(ref returnValue))
+                    {
+                        updatedFlight.Terminal = returnValue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                    break;
+            }
+
 
 
             return false;
         }
+
+        private bool ReadIntValueFromConsole(ref int enteredValue, int exitKey = (int)MainMenuVariants.Exit)
+        {
+            bool customerEnteredCorrectNumber = false;
+            bool customerWantExit = false;
+            int result = -1;
+            do
+            {
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    if (result == exitKey)
+                        customerWantExit = true;
+                    if (result != exitKey)
+                        return true;
+                }
+                catch
+                {   // Writhing warning message:
+                    // Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine($"You have entered wrong value, please try again or enter {exitKey}-if you would like exit from entering the value");
+                    // Console.ForegroundColor = ConsoleColor.Green;
+                }
+            } while (!customerEnteredCorrectNumber && !customerWantExit);
+            return false;
+        }
+
         public Flight GetFlyightByNumber(int number)
         {
 
